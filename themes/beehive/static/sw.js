ef226da9d778d04a3f2f7c97c9c6f268190099a7
vijayabharathi <vijayabharathib@users.noreply.github.com>
2017-12-10 08:14:37 +0530

    fix duplicate network request from sw

--- themes/beehive/static/sw.js
+++ themes/beehive/static/sw.js
@@ -1,57 +1,59 @@
 importScripts("js/cache-polyfill.js");
-var CACHE_KEY='beehive_v12';
+var CACHE_KEY='beehive_v17';
+let files_to_preload=[
+  '/',
+  '/offline/',
+  'img/site_logo.svg',
+  'css/main.css'];
 
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
-//Install stage sets up the offline page in the cahche and opens a new cache
 self.addEventListener('install', function(event) {
   event.waitUntil(preLoad());
 });
 
 var preLoad = function(){
-  return caches.open(CACHE_KEY).then(function(cache) {
-    return cache.addAll(['/offline/', '/index.html']);
+  return caches.open(CACHE_KEY).then(cache => {
+    return cache.addAll(files_to_preload);
   });
 }
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request)}
-  ));
-  event.waitUntil(addToCache(event.request));
+  event.respondWith(returnFromCache(event.request.clone()));
 });
 
-var checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response)
-      } else {
-        reject()
-      }
-    }, reject)
-  });
-};
-
-var addToCache = function(request){
-  return caches.open(CACHE_KEY).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-};
-
 var returnFromCache = function(request){
+  let url=new URL(request.url);
   return caches.open(CACHE_KEY).then(function (cache) {
     return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match('offline/')
-     } else {
-       return matching
-     }
-    });
+      if(!matching || matching.status == 404) {
+        throw Error("no-cache-match");
+      } else {
+        return matching
+      }
+    })
+  }).catch(err=>{
+    return fromNetwork(request);
   });
 };
+
+var offlineContent= function(request){
+  return caches.open(CACHE_KEY).then(function (cache) {
+        return cache.match('offline/')
+  });
+};
+
+var fromNetwork = function(request){
+  let url=new URL(request.url);
+  return fetch(request.clone()).then(function (response) {
+      const chain = Promise.resolve(response.clone());
+      caches.open(CACHE_KEY).then(function (cache) {
+        cache.put(request, response.clone());
+      });
+      return chain;
+    }).catch(err=>{
+      return offlineContent(request);
+    });
+};
+
 
 self.addEventListener("activate", function(event) {
   event.waitUntil(
